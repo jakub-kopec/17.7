62b2e5c69aeef83197df8508784421011c0a87f9
jakub-kopec <agh.jakub@gmail.com>
2019-06-30 21:46:42 +0200

    17.7 ready

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '404449801661-ivk9dc1qoff1jko7u5j6dvdnq5qigbjn.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'AscQfD-_DD29ifSsxLv_pyVh',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+}
